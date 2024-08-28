72d2f4700ab9b79a4a516564288b89b0d25bc994
frani <franinnocenti@gmail.com>
2024-08-28 09:39:31 -0300

    all TCP port open. yolo

--- gen_terraform_aws_ec2_eip.sh
+++ gen_terraform_aws_ec2_eip.sh
@@ -20,22 +20,8 @@ resource "aws_security_group" "allow_tcp" {
   description = "Allow multiple TCP inbound traffic"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 0
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
